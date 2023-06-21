f17b3ac0d2fc0f27a6a3409efb9574f8a7c2f402
Anastasia Sidorenko <anastasia.v.sido@gmail.com>
2023-06-21 21:58:28 +0300

    feat: add about page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,11 @@ export const Header = () => {
   return (
     <div className={styles.headerContainer}>
       <header className={styles.header}>
-        <Link href="/" className={styles.link}>
-          Билетопоиск
-        </Link>
+        <h1>
+          <Link href="/" className={styles.link}>
+            Билетопоиск
+          </Link>
+        </h1>
         <div className={styles.cartWrapper}>
           <Link href="/cart" className={styles.cart}></Link>
         </div>
